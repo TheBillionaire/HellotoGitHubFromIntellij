b76a3558449505ae6b0226acf6c1edc6b4db95a0
Karthick Mulpuri <karthick137@gmail.com>
2021-02-07 20:03:04 +0800

    My Second Commit

--- src/com/karthick/Main.java
+++ src/com/karthick/Main.java
@@ -1,8 +1,12 @@
 package com.karthick;
 
+import com.karthick.model.Person;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from Intellij to GitHub");
+        Person person = new Person("William H", "Gates", 65);
+        System.out.println(person);
     }
 }
